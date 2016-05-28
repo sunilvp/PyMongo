9aa6067d233b61b72a433a803d146d9e938756eb
sunilvp <vip.sunil@gmail.com>
2016-05-28 19:49:36 +0530

    Added Comments

--- WebTest/DbConnection.py
+++ WebTest/DbConnection.py
@@ -2,8 +2,15 @@ import pymongo
 
 from pymongo import MongoClient
 
+#First get connected to the Mongodb
 connection = MongoClient('localhost',27017)
+
+#Once connection is successfull get the db instance
 db= connection.sunil
+
+#From the db get the collections
 names= db.sunilCollections
+
+#Find the item and print the corresponding one
 item = names.find_one()
 print(item['address']) 
--- WebTest/snippet.py
+++ WebTest/snippet.py
@@ -4,4 +4,5 @@ from bottle import route, run, template
 def index(name):
     return template('<b>Hello user {{name}}</b>!', name=name)
 
+#Using Bottle python module to start a webserver and route a simple url example
 run(host='localhost', port=8080)
